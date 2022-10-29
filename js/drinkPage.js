cb2dd7ba526597485ed71117e9eb5ac96c97abd4
Moheud Malic <mohidalamsohail22@live.com>
2022-10-29 21:27:38 +0300

    initial commit

---
+++ js/drinkPage.js
@@ -0,0 +1,55 @@
+// getting drink id as passed in the link  
+let params = new URLSearchParams(window.location.search)
+let drinkId = params.get("iddrink")
+
+console.log(drinkId)
+
+// getting important elements
+let title = document.getElementById("drinkTitle")
+let image = document.getElementById("drinkImg")
+let ingredientsList = document.getElementById("drinkIngredients")
+let instructions =  document.getElementById("drinkInstructions")
+
+
+// creating params to be passed in the request
+let newparams = new URLSearchParams({
+    "i": drinkId
+})
+
+// making request to get drink data 
+fetch("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?"+newparams).then((resp) => {
+    if (resp.ok) {
+        return resp.json()
+    }
+    else 
+    {
+        console.log("Sorry we could not get the data from the server",  resp.status)
+    }
+}).then(
+    (data) => {
+
+        // scrapping json to get the drink data 
+        let drink = data.drinks[0]
+         
+        // putting data in dom 
+        title.innerText = drink.strDrink
+        image.src = drink.strDrinkThumb
+        instructions.innerText = drink.strInstructions
+
+        // putting all the ingredients in an array
+        let ingredientsArray = []
+        let counter = 1
+        while(drink["strIngredient"+counter] != null || drink["strIngredient"+counter] != undefined) {
+            ingredientsArray.push(drink["strIngredient"+counter])
+            counter++
+         }
+
+        // putting all ingredients as list items in ingredients list in dom
+        ingredientsArray.forEach((ingredient) => {
+            let listItem = document.createElement("li")
+            listItem.innerText = ingredient
+            ingredientsList.appendChild(listItem)
+        })
+
+    }
+)
---
+++ js/main.js
@@ -0,0 +1,101 @@
+
+// getting main useful elements
+let searchBox = document.getElementById("searchBox") 
+let isAlcoholicCheck = document.getElementById("alcoholicCheckBox")
+let resultsList = document.getElementById("resultsList")
+
+// an array of drinks
+let drinks = []
+
+
+// A function that pushes the drinks into results list. 
+function pushDrinks(drinks) {
+    // drinks is an array of drinks that came from the api, and we get it in the function below.
+    // drinks can also be null if the search returns null from the api. 
+
+    if (drinks == null || drinks.length == 0) {
+        resultsList.innerHTML = "<h3>Sorry, Your search brought no results</h3>"
+    } 
+    else 
+    {
+        drinks.forEach(drink => {
+
+            // creating elements that represent each element. 
+            let listItem = document.createElement("li")
+            let listItemImage = document.createElement("img")
+            let listItemName = document.createElement("h4")
+
+            
+            listItem.onclick = () => {location.href = './drink.html?iddrink='+drink.idDrink } 
+
+
+            // giving attribute values to elements 
+            listItemImage.src = drink.strDrinkThumb
+            listItemName.innerText = drink.strDrink
+           
+    
+        
+            // adding image, name and button to the list item
+            listItem.appendChild(listItemImage)
+            listItem.appendChild(listItemName)
+            
+    
+            // adding the list item to the list 
+            resultsList.appendChild(listItem);
+        });
+    }
+   
+}
+
+
+// The function that makes the api call
+function getDrinks() {
+    let query = searchBox.value
+    let urlParams = new URLSearchParams({
+        "s": query
+    })
+
+    // getting from the api
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?"+urlParams).then((resp) => {
+        if (resp.ok) {
+            return resp.json()
+        }
+        else {
+            console.log("There was some error in getting the data:", resp.status)
+        }
+    }).then((data) => {
+         //gettings drinks array from data  
+          drinks = data.drinks 
+          let isAlcoholic = !isAlcoholicCheck.checked
+
+          //resetting the last results list   
+          resultsList.innerHTML = ""
+
+          //checking for alchololic filter and filtering.
+          if (isAlcoholic == true) {
+            pushDrinks(drinks)
+          }
+          else 
+          {
+            drinks = drinks.filter((drink) => {
+                if (drink.strAlcoholic == "Alcoholic") {
+                    return false 
+                }
+                else {
+                    return true
+                }
+            } ) 
+
+           pushDrinks(drinks)
+            
+          }
+    }).catch((err) => {
+        console.log("Sorry there was an error in trying to get the data", err)
+    })
+}
+
+
+// executing get drinks function on these events 
+searchBox.oninput = getDrinks
+isAlcoholicCheck.onchange = getDrinks
+getDrinks()
